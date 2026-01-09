58a6d5bcdff9164ae64dbd5f59e47b9850427746
MCV <marie-claire.vogt@orange.fr>
2026-01-09 19:55:07 +0100

    modifications

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function submit(){
-    //récupérer clair ou obscur
+    //récupérer le thème du site clair ou obscur
+
+    if (document.getElementById("theme")){
+
     let theme = document.getElementById("theme").value;
     console.log(theme);
     //afficher dans le stockage local
     localStorage.setItem("theme", theme);
-    //rafraichir pour declencher changement de couleur
-    initiale();
-
+    //rafraîchir pour déclencher le changement de couleur
+  }
 
     //récupérer l'élément coché entre liste et cartes
     let affichage = "Liste";
@@ -15,11 +17,10 @@ function submit(){
     console.log(affichage);
     }
     localStorage.setItem("affichage", affichage);
-    //rafraichir 
+    //rafraîchir 
     initiale();
 }
-// autre solution:  document.getElementById('submitId').on('clic', function() {
-    //actions});
+// autre solution:  document.getElementById('submitId').on('clic', function() {actions});
 
 function initiale(){
 
@@ -35,7 +36,7 @@ function initiale(){
         console.log(body.style);
     }else{
 
-        body.style.backgroundColor = "lightcyan";
+        body.style.backgroundColor = "rgb(176, 201, 247)";
         body.style.color = "#000"; 
     }
 }
@@ -46,15 +47,10 @@ function switch_accueil () {
         affichage = localStorage.getItem("affichage");//va chercher la valeur en face de la clé affichage
     }
 
-
     if(affichage=="Cartes"){
        
        window.location.replace("index_cartes.html"); //je change de page pour accueil avec cartes
-        
-    
-    
     }
-  
 }
 
 //Pour mettre les données du promo.json dans les cartes d'index_cartes.html
@@ -98,7 +94,7 @@ function fetch_apprenants_liste(){
  
                     const detailsStudent = document.createElement('tr');//1 ligne pour chaque apprenant
 
-                    detailsStudent.innerHTML = "<td>" + student.prenom +"</td> <td>" + student.nom + "</td> <td> " + student.ville + "</td> <td> <a href='#' >Détail</a></td>";//innerhtml=contenu
+                    detailsStudent.innerHTML = "<td>" + student.prenom +"</td> <td>" + student.nom + "</td> <td> " + student.ville + "</td> <td> <a class='details' href='#dialog'>Détail</a></td>";//innerHTML=contenu
                     conteneur.appendChild(detailsStudent);//j'ajoute au tableau vide tbody chaque ligne d'apprenants
             }
               });
@@ -109,6 +105,83 @@ function autre_relie(){
     fetch_apprenants_liste();
 
 }
+
+/*MODALE POP-UP DIALOG essai 1
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// Le bouton "Afficher la fenêtre" ouvre le dialogue
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// Le bouton "Fermer" ferme le dialogue
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});*/
+
+/*MODALE POP-UP DIALOG essai 2*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll("a.Detail");
+  const modal = document.getElementById("modal");
+  const closeBtn = document.querySelector(".close");
+
+  links.forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault(); // empêche le lien de naviguer
+      modal.style.display = "block";
+    });
+  });
+
+  closeBtn.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+/*fin MODALE POP-UP DIALOG essai 2*/
+
+
+/*ESSAI 3
+
+Pour mettre les données du promo.json dans la modale, le pop-up de index.html
+ function fetch_apprenants(){
+
+    fetch('promo.json')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+
+            let conteneur = document.getElementById('donnees_modale');
+            for (student of data.apprenants) { //student=chaque élément du tableau apprenants
+ 
+                    const detailsStudent = document.createElement('h2');
+
+                    detailsStudent.innerHTML = "<br>" + student.nom + '<br>' + student.prenomnom + "<br>" + "<br>" + student.ville + "<br>" + student.coordonnees.latitude + "<br>" + student.coordonnees.longitude +"<br>" ;
+                    conteneur.appendChild(detailsStudent);
+            }
+              });
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*ESSAI AVEC TP DU COURS JSON
@@ -121,7 +194,7 @@ function displayData(data){
 }
 */
 
-//relier avec id map sur map.html
+/*relier avec id map sur map.html
 let lat = 48.852969;
 let lon = 2.349903;
 
@@ -137,4 +210,4 @@ L.tileLayer("https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
 
 }).addTo(map);
 
-let marker = L.marker([lat,lon]).addTo(map);
+let marker = L.marker([lat,lon]).addTo(map);*/
